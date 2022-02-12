45e48ebf806086eaf66a0c7116dba1499485c31b
Matthew Downs <matthew@neer.ai>
2022-02-12 09:40:48 -0600

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,38 @@ const { join } = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-module.exports = {
-    target: 'web',
-    entry: join(__dirname, './src/index.tsx'),
-    output: {
-        filename: '[name].js',
-        path: join(__dirname, './dist'),
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: ['ts-loader']
-            }
-        ]
-    },
-    plugins: [
-        new NodePolyfillPlugin(),
-        new HtmlPlugin({
-            template: join(__dirname, './src/index.ejs'),
-            templateParameters: {
-                base: '/zip-test'
-            }
-        })
-    ],
-    resolve: {
-        extensions: [ '.js', '.json', '.ts', '.tsx']
-    },
-    devtool: 'source-map',
-    devServer: {
-        port: 8080
-    }
+module.exports = (env) => {
+    return {
+        target: 'web',
+        entry: join(__dirname, './src/index.tsx'),
+        output: {
+            filename: '[name].js',
+            path: join(__dirname, './dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: ['ts-loader']
+                }
+            ]
+        },
+        plugins: [
+            new NodePolyfillPlugin(),
+            new HtmlPlugin({
+                template: join(__dirname, './src/index.ejs'),
+                templateParameters: {
+                    base: env.base || '/'
+                }
+            })
+        ],
+        resolve: {
+            extensions: ['.js', '.json', '.ts', '.tsx']
+        },
+        devtool: 'source-map',
+        devServer: {
+            port: 8080
+        }
+    };
 };
